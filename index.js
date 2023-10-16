5eaf390224c3776ae3680e654e5e1ec8c4876ddf
Christopher <raphelchris@gmail.com>
2023-10-16 15:57:09 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+
+
+
+function writeCards(names, eventName) {
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+       
+    }
+
+    return messages;
+}
+
+function countDown(positiveInteger) {
+    while (positiveInteger >= 0) {
+        console.log(positiveInteger--)
+    }
+
+    
+    
+     
+}
+
+
+countDown(25);
+
+
+
+
+
+
+
+
+
